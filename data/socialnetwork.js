80c3380217fb6393c02a6b319eba5660331e181e
Alex Bonilla <alex@cloudbrix.com>
2021-10-06 11:03:08 -0500

    Fixed grammar error.

--- data/socialnetwork.js
+++ data/socialnetwork.js
@@ -3,7 +3,7 @@ const socialnetwork = [
       name: 'Instagram',
       slug: 'instagram',
       html_url: 'https://www.instagram.com/alexfbonilla/',
-      description: 'Pictures of my children, videos of songs I learn on guitar and anything that catch my eyes everynow and then.'
+      description: 'Pictures of my children, videos of songs I learn on guitar and anything that catches my attention everynow and then.'
     },
     {
       name: 'LinkedIn',
