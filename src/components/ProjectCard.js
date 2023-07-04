972cd576f0a38bb1e7c42c987eb8bb2e67ef1df3
Alex Bonilla <afbonilla@gmail.com>
2023-07-04 00:03:33 -0400

    Add Homepage URL and topics to Project Card

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -6,13 +6,18 @@ export default function ProjectCard (props) {
         <div className="content">
           <h3 className="title is-12"><a href={repo.html_url} target="_new">{repo.name}</a></h3>
           <p>{repo.description !== null?repo.description:"No description added yet."}</p>
-          {repo.homepage!=null?(<a href={repo.homepage}>Homepage</a>):(<br/>)}
+          {repo.homepage!=null?(<div className="homepage_url"><a href={repo.homepage} target="_blank">Homepage</a></div>):(<br/>)}
           <label>Stargazers:</label> <span>{repo.stargazers_count}</span>
           <br/>
           {
             repo.language &&  <span className="tag my-3">{repo.language}</span>
           }
           <br/>
+          <ul>
+            {repo.topics & repo.topics.map((item, index) => (
+              <li key={index}>{item}</li>
+            ))}
+          </ul>
           <time>{repo.created_at}</time>
         </div>
       </div>
