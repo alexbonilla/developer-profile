7460b3c7b46902d1652cc27e12a156c7fff52bc9
Alex Bonilla <afbonilla@gmail.com>
2023-07-03 23:45:06 -0400

    Add Homepage Link to Project Card (fix 2)

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -6,7 +6,7 @@ export default function ProjectCard (props) {
         <div className="content">
           <h3 className="title is-12"><a href={repo.html_url} target="_new">{repo.name}</a></h3>
           <p>{repo.description !== null?repo.description:"No description added yet."}</p>
-          {repo.homepage!=null?(<a href='{repo.homepage}'>Homepage</a>)}
+          {repo.homepage!=null?(<a href='{repo.homepage}'>Homepage</a>):(<br/>)}
           <label>Stargazers:</label> <span>{repo.stargazers_count}</span>
           <br/>
           {
