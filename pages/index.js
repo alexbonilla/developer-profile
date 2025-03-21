fd610b11ed6bff32555e2eb1e41b89838bd89ae7
Alex Bonilla <afbonilla@gmail.com>
2025-03-21 11:35:32 -0400

    Removed call to Credly api

--- pages/index.js
+++ pages/index.js
@@ -31,10 +31,7 @@ export default function Home() {
         console.log('An error ocurred while requesting for user data.');
         setLoading(false);
       });
-  }, []);
-  
-
-  
+  }, []);  
 
   useEffect(() => {
 
