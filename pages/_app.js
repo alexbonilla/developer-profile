d519f4b6b7db4702f21663ac6d63cfb536e37016
Alex Bonilla <afbonilla@gmail.com>
2025-03-21 11:41:40 -0400

    Removed Google tag

--- pages/_app.js
+++ pages/_app.js
@@ -6,23 +6,7 @@ function MyApp({ Component, pageProps }) {
   const YOUR_GTM_ID = 'G-FM8VDJSKVE'
   return (
     <>
-      <Component {...pageProps} />
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${YOUR_GTM_ID}`}
-      />
-      <Script
-        id="google-tag-manager"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${YOUR_GTM_ID}');
-          `,
-        }}
-      />
+      <Component {...pageProps} />      
     </>
   )
 }
